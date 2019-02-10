32f049d4af1eff157d8566f6d56a315adb550696
Ali Shokoohi <alishokoohi666@gmail.com>
2019-02-10 22:26:02 +0330

    Define /start command

---
+++ bot/bot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackQueryHandler
+from telegram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from datetime import datetime
+import logging
+
+home_dir = '../'#Bot's home loction.
+log_location =  home_dir+'logs/bot.log'#Loction of log file.
+logging.basicConfig(filename=log_location ,format='%(asctime)s - %(name)s - %(message)s', level=logging.INFO)
+
+#Bot token.
+token = "777099556:AAGUJtwLihGVQk2aAt9TgeVEXPf4aCMAy-U"
+updater = Updater(token=token)
+
+class User:
+    def __init__(self):
+        self.id = None
+        self.chat = None
+        self.fname = None
+        self.lname = None
+        self.uname = None
+
+user = User()
+
+# Start function for answer /start command.
+def start(bot, update):
+    user.chat = update.message.chat_id #Get chat ID.
+    text = "سلام. برای دیدن لیست دستورات، دستور زیر را ارسال نمیایید:\n/help"
+    bot.send_message(chat_id=user.chat, text=text)#Send text message to chat ID.
+
+
+dispatcher = updater.dispatcher
+
+start_command = CommandHandler('start', start)
+dispatcher.add_handler(start_command) #Add command handler /start.
+
+print("READY!")
+
+#Start polling
+updater.start_polling()
+updater.idle()
